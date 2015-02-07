6282d5269d8af18a99227bebca6386327e836ee2
tako-black <gennarishock+tako@gmail.com>
2015-02-07 17:21:59 +0900

    1.3.2

--- xhr-promise.js
+++ xhr-promise.js
@@ -4,21 +4,26 @@ function getURL(URL) {
 	return new Promise(function (resolve, reject) {
 		http.get(URL, function (res) {
 			var body = '';
+			var ret = '';
 			res.setEncoding('utf8');
 			res.on('data', function (chunk) {
 				body += chunk;
 			});
 			res.on('end', function () {
 				ret = JSON.parse(body);
-				resolve(ret);
 			});
+			if (res.statusCode === 200) {
+				resolve(ret);
+			} else {
+				reject(new Error(res.statusText));
+			}
 		}).on('error', function (e) {
 			reject(new Error(e));
 		});
 	});
 };
 
-var URL = 'http://httpbin.org/get';
+var URL = 'http://httpbin.org/status/500';
 getURL(URL).then(function onFulfilled(value) {
 	console.log(value);
 }).catch(function onRejected(error) {
