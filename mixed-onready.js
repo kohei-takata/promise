f0d0880c619d414ac219e30eaa7a4073b389d2aa
tako-black <gennarishock+tako@gmail.com>
2015-02-08 11:39:49 +0900

    2.3

---
+++ mixed-onready.js
@@ -0,0 +1,13 @@
+function onReady(fn) {
+	var readyState = document.readyState;
+	if (readyState === 'interactive' || readyState === 'complete') {
+		fn();
+	} else {
+		window.addEventListener('DOMContentLoaded', fn);
+	}
+}
+
+onReady(function () {
+	console.log('DOM fully loaded and parsed');
+});
+console.log('==Starting==');
